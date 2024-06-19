4bf0a7635fd96347365fa7bae619e8c21e2c1b8f
Alex <alex.bataille@ulb.be>
2024-06-19 10:53:51 +0200

    added new windows and buttons like the menu or the different sorting buttons

--- src/main.py
+++ src/main.py
@@ -1,16 +1,61 @@
 import tkinter
 
+
+def create_window(title : str, dimensions : str, bg_color : str):
+    generic_window = tkinter.Tk()
+    generic_window.title(title)
+    generic_window.geometry(dimensions)
+    generic_window.configure(bg=bg_color)
+    generic_window.resizable(False, False)
+
+    title_text = tkinter.StringVar()
+    title_text.set(title)
+    title_label = tkinter.Label(generic_window, textvariable=title_text, bg="black", fg="white", font=("Arial", 30))
+    title_label.pack(pady=20)
+
+    menu_button = tkinter.Button(generic_window, text="Menu", command=lambda: [generic_window.destroy(), main()])
+    menu_button.place(x=10, y=10)
+    return generic_window
+
+
+def sorting_algo(win : tkinter.Tk):
+    win.destroy()
+
+    sorting_window = create_window('Sorting Algorithms', '1200x900', 'black')
+
+    sorting_menu_button = tkinter.Button(sorting_window, )
+
+
+    selection_sort_button = tkinter.Button(sorting_window, text="Selection Sort", width=20, height=5)
+    selection_sort_button.place(x=100, y=100)
+
+    insertion_sort_button = tkinter.Button(sorting_window, text="Insertion Sort", width=20, height=5)
+    insertion_sort_button.place(x=400, y=100)
+
+    sorting_window.mainloop()
+
+
+def pathfinding_algo(win : tkinter.Tk):
+    win.destroy()
+
+    pathfinding_window = create_window('PathFinding Algorithms', '1200x900', 'black')
+
+    pathfinding_window.mainloop()
+
+
+def main():
+    main_window = create_window("Algorithm Visualisation App (made by Alex Bataille)", "1200x900", "black")
+
+
+    sorting_button = tkinter.Button(main_window, text="Sorting", width=20, height=5,
+                                   command=lambda: sorting_algo(main_window))
+    sorting_button.place(x=100, y=100)
+
+    path_finding_button = tkinter.Button(main_window, text="PathFinding", width=20, height=5,
+                                       command=lambda: pathfinding_algo(main_window))
+    path_finding_button.place(x=400, y=100)
+
+    main_window.mainloop()
+
 if __name__ == "__main__":
-    window = tkinter.Tk()
-    window.title("Algorithm Visualisation App")
-    window.geometry("1200x900")
-    window.resizable(False, False)
-    window.configure(background="black")
-
-    sortingFrame = tkinter.Frame(window, bg="red")
-    sortingFrame.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-
-    sortingButton = tkinter.Button(sortingFrame, text="Sorting")
-    sortingButton.pack(side=tkinter.TOP)
-
-    window.mainloop()
+    main()
