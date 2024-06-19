94d988c60262eaeffe718507e2d4f5e88afa3340
Alex <alex.bataille@ulb.be>
2024-06-19 13:42:48 +0200

    added display of shuffled list

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
 import tkinter
+import random
 
 
-def create_window(title : str, dimensions : str, bg_color : str):
+def shuffle_list(win : tkinter.Tk) -> list[int]:
+    canvas = tkinter.Canvas(win, width=900, height=900, bg="black")
+    canvas.pack(side=tkinter.LEFT)
+    allLines = []
+    for i in range(1, 900):
+        allLines.append(i)
+    random.shuffle(allLines)
+    for index in range(len(allLines)):
+        canvas.create_line(1 + index, 900, 1 + index, allLines[index], fill="white", width=1)
+    return allLines
+
+def selection(win : tkinter.Tk):
+    win.destroy()
+
+    selection_window = create_window("Selection Sort", "1200x900", "black")
+    all_lines = shuffle_list(selection_window)
+    selection_window.mainloop()
+
+
+def create_window(title : str, dimensions : str, bg_color : str) -> tkinter.Tk:
     generic_window = tkinter.Tk()
     generic_window.title(title)
     generic_window.geometry(dimensions)
@@ -23,10 +43,7 @@ def sorting_algo(win : tkinter.Tk):
 
     sorting_window = create_window('Sorting Algorithms', '1200x900', 'black')
 
-    sorting_menu_button = tkinter.Button(sorting_window, )
-
-
-    selection_sort_button = tkinter.Button(sorting_window, text="Selection Sort", width=20, height=5)
+    selection_sort_button = tkinter.Button(sorting_window, text="Selection Sort", width=20, height=5, command=lambda: selection(sorting_window))
     selection_sort_button.place(x=100, y=100)
 
     insertion_sort_button = tkinter.Button(sorting_window, text="Insertion Sort", width=20, height=5)
